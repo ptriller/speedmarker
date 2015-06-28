657574a64c5a72886dfb896963d8d042e8ffbbed
Peter Triller <ptriller@soapwars.de>
2015-06-29 01:21:44 +0200

    complete tokenizer ?

---
+++ src/main/java/de/soapwars/speedmarker/StateBean.java
@@ -0,0 +1,16 @@
+package de.soapwars.speedmarker;
+
+/**
+ * Created by ptriller on 29.06.2015.
+ */
+public class StateBean {
+
+    public int state;
+
+    public int curly;
+
+    public StateBean(int state, int curly) {
+        this.state = state;
+        this.curly = curly;
+    }
+}
