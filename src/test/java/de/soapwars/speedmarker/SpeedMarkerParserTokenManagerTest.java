1f02e73ce1d97c98dceca3e8b133c587f071d85a
Peter Triller <ptriller@soapwars.de>
2015-06-29 02:49:45 +0200

    complete tokenizer ?

--- src/test/java/de/soapwars/speedmarker/SpeedMarkerParserTokenManagerTest.java
+++ src/test/java/de/soapwars/speedmarker/SpeedMarkerParserTokenManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.StringReader;
 
+import static de.soapwars.speedmarker.SpeedMarkerParserConstants.*;
 import static org.junit.Assert.*;
 
 /**
@@ -13,15 +14,31 @@ public class SpeedMarkerParserTokenManagerTest {
 
     @Test
     public void testTokenizer1() throws Exception {
-        SimpleCharStream stream = new SimpleCharStream(new StringReader("<#assign test />"));
-        SpeedMarkerParserTokenManager manager = new SpeedMarkerParserTokenManager(stream);
-        assertEquals(SpeedMarkerParserConstants.DIRECTIVE_START, manager.getNextToken().kind);
-        assertEquals(SpeedMarkerParserConstants.ASSIGN, manager.getNextToken().kind);
-        assertEquals(SpeedMarkerParserConstants.IDENTIFIER, manager.getNextToken().kind);
-        assertEquals(SpeedMarkerParserConstants.EMPTY_TAG, manager.getNextToken().kind);
-        assertEquals(SpeedMarkerParserConstants.EOF, manager.getNextToken().kind);
+        validateTokenStream("<#assign test />",
+                DIRECTIVE_START, ASSIGN, IDENTIFIER, EMPTY_TAG, EOF);
     }
 
+    @Test
+    public void testTokenizer2() throws Exception {
+        validateTokenStream("xxx${{}}yyy",
+                CONTENT, EXPRESSION_START, CURLYOPEN, CURLYCLOSE, CURLYCLOSE, CONTENT, EOF);
+    }
+
+    @Test
+    public void testTokenizer3() throws Exception {
+        validateTokenStream("xxx${\"Hallo ${du} da\"}yyy",
+                CONTENT, EXPRESSION_START, DOUBLE_STRINGSEGMENT, STRING_EXPRESSION_START
+                , IDENTIFIER, CURLYCLOSE, DOUBLE_STRINGSEGMENT, END_DOUBLE_STRING,
+                CURLYCLOSE, CONTENT, EOF);
+    }
+
+    @Test
+    public void testTokenizer4() throws Exception {
+        validateTokenStream("xxx${\'Hallo ${du} da\'}yyy",
+                CONTENT, EXPRESSION_START, SINGLE_STRINGSEGMENT, STRING_EXPRESSION_START
+                , IDENTIFIER, CURLYCLOSE, SINGLE_STRINGSEGMENT, END_SINGLE_STRING,
+                CURLYCLOSE, CONTENT, EOF);
+    }
     @Test
     public void testContentMerge() throws Exception {
         SimpleCharStream stream = new SimpleCharStream(new StringReader("test<me"));
@@ -37,9 +54,21 @@ public class SpeedMarkerParserTokenManagerTest {
         SpeedMarkerParserTokenManager manager = new SpeedMarkerParserTokenManager(stream);
         validateNextToken(manager, SpeedMarkerParserConstants.CONTENT, "x");
         validateNextToken(manager, SpeedMarkerParserConstants.EXPRESSION_START, "${");
-        validateNextToken(manager, SpeedMarkerParserConstants.IDENTIFIER, "test");
+        validateNextToken(manager, IDENTIFIER, "test");
         validateNextToken(manager, SpeedMarkerParserConstants.CURLYCLOSE, "}");
         validateNextToken(manager, SpeedMarkerParserConstants.CONTENT, "y");
+    }
+
+    private void validateTokenStream(String input, int... tokens) {
+        SimpleCharStream stream = new SimpleCharStream(new StringReader(input));
+        SpeedMarkerParserTokenManager manager = new SpeedMarkerParserTokenManager(stream);
+        int i = 0;
+        for (int token : tokens) {
+            Token tk = manager.getNextToken();
+            assertEquals("Failed at Token " + i + ' ' + tk,
+                    token, tk.kind);
+            ++i;
+        }
     }
 
     private void validateNextToken(SpeedMarkerParserTokenManager manager, int kind, String value) {
